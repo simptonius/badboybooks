e5f501558951865d6932020ee03e21523ca0d8b0
simptonius <edwardjmendenhall@gmail.com>
2026-03-30 20:47:31 -0700

    Update book.js

--- book.js
+++ book.js
@@ -68,7 +68,7 @@
     }
   );
 
-  var drag=false, px=0, py=0, vy=0.0005, vx=0, ry=2.15, rx=-0.3;
+  var drag=false, px=0, py=0, vy=0.0005, vx=0, ry=2.35, rx=-0.35;
   canvas.addEventListener('mousedown', function(e){drag=true;px=e.clientX;py=e.clientY;});
   window.addEventListener('mousemove', function(e){
     if(!drag)return; vy=(e.clientX-px)*.003; vx=(e.clientY-py)*.003;
